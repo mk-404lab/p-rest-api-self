e4a0cc96ecd273b2b896f6c5d52410dbcf2c665b
mk-404lab <alsgur9984@naver.com>
2026-03-16 10:09:05 +0900

    feat: 11강 - 글 단건 조회, 댓글 단건, 다건 조회 구현

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -6,17 +6,21 @@ import com.back.domain.post.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/posts")
 public class ApiV1PostController {
 
     private final PostService postService;
 
-    @GetMapping("/api/v1/posts")
+    // 글 다건 조회
+    @GetMapping
     @ResponseBody
     public List<PostDto> list() {
         List<Post> result = postService.findAll();
@@ -31,5 +35,14 @@ public class ApiV1PostController {
          */
 
         return postDtoList;
+    }
+
+    // 글 단건 조회
+    @GetMapping("/{id}")
+    @ResponseBody
+    public PostDto detail(@PathVariable int id) {
+        Post post = postService.findById(id).get();
+
+        return new PostDto(post);
     }
 }
