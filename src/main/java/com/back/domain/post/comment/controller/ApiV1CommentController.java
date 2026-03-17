dcac724a4f0123b9030815e5dc16def12eeefbf1
mk-404lab <alsgur9984@naver.com>
2026-03-17 14:42:20 +0900

    feat: 29강 - 댓글 등록 API 구현

--- src/main/java/com/back/domain/post/comment/controller/ApiV1CommentController.java
+++ src/main/java/com/back/domain/post/comment/controller/ApiV1CommentController.java
@@ -5,14 +5,16 @@ import com.back.domain.post.comment.entity.Comment;
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
 import com.back.global.rsData.RsData;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/posts/{postId}/comments")
 public class ApiV1CommentController {
@@ -21,7 +23,6 @@ public class ApiV1CommentController {
 
     // 댓글 다건 조회
     @GetMapping
-    @ResponseBody
     public List<CommentDto> list(
             @PathVariable int postId
     ){
@@ -37,7 +38,6 @@ public class ApiV1CommentController {
 
     // 댓글 단건 조회
     @GetMapping("/{commentId}")
-    @ResponseBody
     public CommentDto detail(@PathVariable int postId, @PathVariable int commentId) {
         Post post = postService.findById(postId).get();
         Comment comment = post.findCommentById(commentId).get();
@@ -47,7 +47,6 @@ public class ApiV1CommentController {
 
     // 댓글 삭제
     @DeleteMapping("{commentId}")
-    @ResponseBody
     @Transactional
     public RsData<Void> delete(
             @PathVariable int postId,
@@ -60,6 +59,38 @@ public class ApiV1CommentController {
         return new RsData<>(
                 "%d번 댓글이 삭제되었습니다.".formatted(commentId),
                 "204-1"
+        );
+    }
+
+
+    record CommentWriteReqBody(
+
+            @NotBlank(message = "02-content-내용은 필수입니다.")
+            @Size(min = 2, max = 100, message = "04-content-내용은 2자 이상 100자 이하로 입력해주세요.")
+            String content
+    ){}
+
+    record CommentWriteResBody(
+            CommentDto commentDto
+    ){}
+
+    @PostMapping
+    @Transactional
+    public RsData<CommentWriteResBody> write(
+            @PathVariable int postId,
+            @RequestBody @Valid CommentWriteReqBody reqBody
+    ) {
+        Post post = postService.findById(postId).get();
+        Comment comment = post.addComment(reqBody.content);
+
+        postService.flush();
+
+        return new RsData<>(
+                "%d번 댓글이 성공적으로 작성되었습니다.".formatted(comment.getId()),
+                "201-1",
+                new CommentWriteResBody(
+                        new CommentDto(comment)
+                )
         );
     }
 }
--- src/main/java/com/back/domain/post/post/service/PostService.java
+++ src/main/java/com/back/domain/post/post/service/PostService.java
@@ -42,4 +42,8 @@ public class PostService {
     public List<Post> findAll() {
         return postRepository.findAll();
     }
+
+    public void flush() {
+        postRepository.flush();
+    }
 }
